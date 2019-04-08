7d169e73cba40b9bd118ed897cfb8425afe5ef0e
Eduardo M <emartinez05@manhattan.edu>
2019-04-08 18:00:58 -0400

    Update AppDelegate.swift

--- SnapTrack/SnapTrack/AppDelegate.swift
+++ SnapTrack/SnapTrack/AppDelegate.swift
@@ -52,6 +52,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    // pulled from my ass i mean stack overflow
+    func strHash(_ str: String) -> UInt64 {
+        var result = UInt64 (5381)
+        let buf = [UInt8](str.utf8)
+        for b in buf {
+            result = 127 * (result & 0x00ffffffffffffff) + UInt64(b)
+        }
+        return result
+    }
 
     
     
