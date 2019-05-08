f67ae4f34a7325f1ad80efe9a55a6ba5da9fb119
Emmanuel Sanchez <esanchez01@manhattan.edu>
2019-05-08 18:53:17 -0400

    Update ViewController.swift

--- SnapTrack/SnapTrack/ViewController.swift
+++ SnapTrack/SnapTrack/ViewController.swift
@@ -9,53 +9,6 @@
 import UIKit
 
 var mainUser = UserInfo()
-/*
-func sendRequest(url: String, route: String, params: [String]) -> [String : Any] {
-    // URL=ServerUrl ; route = serverside function to execute (i.e. login, signup) ; params=http post/get data to send (format ["name1=value", "name2=value", ... ])
-    print(url)
-    print(route)
-    print(params.joined(separator: "&"))
-    let base_url = URL(string: String(format: "%@/%@", url, route))
-    var literallyAnything : [String : Any]
-    literallyAnything = ["message" : "wait"]
-    var request = URLRequest(url: base_url!)
-    let bodyString = params.joined(separator: "&")
-    let bodyData = bodyString.data(using: String.Encoding.utf8)
-    
-    
-    request.httpBody = bodyData
-    
-    print(NSString(data: request.httpBody!, encoding: String.Encoding.utf8.rawValue)!)
-    
-    request.httpMethod = "POST"
-    
-    request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
-    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.addValue("application/json", forHTTPHeaderField: "Accept")
-    let task = URLSession.shared.dataTask(with: request) { data, response, error in guard let data = data,
-        let response = response as? HTTPURLResponse,
-        (200 ..< 300) ~= response.statusCode,
-        error == nil else {
-            print ("unable to connect")
-            return
-        }
-        
-        let responseObject = ((try? JSONSerialization.jsonObject(with: data, options: [])) as? [String: Any])!
-        literallyAnything = responseObject
-        
-    }
-    task.resume()
-    let _timeout = Date().timeIntervalSince1970 + 5
-    while (literallyAnything["message"] as! String == "wait") {
-        if (Date().timeIntervalSince1970 > _timeout){
-            break
-        }
-    }//wait for response
-    return literallyAnything
-}
-
-*/
-
 
 class ViewController: UIViewController {
 
@@ -63,9 +16,6 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
     var currentUserFirstName:String!
-    
-    // pulled from my ass i mean stack overflow
-
     
     override func shouldPerformSegue(withIdentifier identifier: String?, sender: Any?) -> Bool {
         if let ident = identifier {
@@ -83,13 +33,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
         
-        //view.addGestureRecognizer(tap)
     }
     
-    // trying to dissmiss bitchass keyboard
+        //dismiss keyboard (does not work!)   
         func textFieldShouldReturn(textField: UITextField) {
             
             //userNameTextField.resignFirstResponder()
@@ -113,75 +60,7 @@ class ViewController: UIViewController {
             else{
                 let hashValue = strHash(str: String(format: "%@%@", user_!, pass_!))
                 print(hashValue)
-                /*
-                let base_url = URL(string: String (format:"http://%@/login", server1))
                 
-                //base_url?.queryItems = [
-                  //  URLQueryItem(name: "user", value: user_),
-                  //  URLQueryItem(name: "pass", value: String(format: "'%@'", String(hashValue)))
-                //]
-                
-                var request = URLRequest(url: base_url!)
-                let bodyString = String(format: "user=%@&pass=%@", user_!, String(hashValue))
-                let bodyData = bodyString.data(using: String.Encoding.utf8)
-                
-                
-                request.httpBody = bodyData
-                
-                print(NSString(data: request.httpBody!, encoding: String.Encoding.utf8.rawValue)!)
-                
-                request.httpMethod = "POST"
-                
-                request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
-                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-                request.addValue("application/json", forHTTPHeaderField: "Accept")
-                let task = URLSession.shared.dataTask(with: request) { data, response, error in guard let data = data,
-                    let response = response as? HTTPURLResponse,
-                    (200 ..< 300) ~= response.statusCode,
-                    error == nil else {
-                        print (base_url?.description as Any)
-                        return
-                    }
-                    let responseObject = (try? JSONSerialization.jsonObject(with: data, options: .mutableContainers)) as? [String: Any]
-                    // let responseMessage = String(format: "%@", responseObject?["response"] as! CVarArg)
-                    let responseMessage = String(format: "%@", responseObject?["message"] as! CVarArg)
-                 
-                 
-                    if (responseMessage == "failed"){
-                        print(String(format: "%@\n", responseMessage))
-                 
-                        /*let alertLogin = UIAlertController(title: "ERROR: Wrong Username Or Password", message: "Please Try Again", preferredStyle: .alert)
-                 
-                        alertLogin.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
-                 
-                        self.present(alertLogin, animated: true)*/
-                        return
-                    }
-     
-                    mainUser.email = String(format: "%@", responseObject?["email"] as! CVarArg)
-                    mainUser.fname = String(format: "%@", responseObject?["fname"] as! CVarArg)
-                    mainUser.lname = String(format: "%@", responseObject?["lname"] as! CVarArg)
-                    mainUser.dateOfBirth = String(format: "%@", responseObject?["dob"] as! CVarArg)
-                    mainUser.weight = String(format: "%@", responseObject?["weight"] as! CVarArg)
-                    mainUser.height = String(format: "%@", responseObject?["height"] as! CVarArg)
-                 
-                    print(String(format: "name:%@ %@\n", mainUser.fname, mainUser.lname))
-                    print(String(format: "date of birth: %@\n", mainUser.dateOfBirth))
-                 
-                 
-                    //let IconsTabViewController = self.storyboard?.instantiateViewController(withIdentifier: "iconsTab") as! IconsTabViewController
-                    //self.navigationController?.pushViewController(IconsTabViewController, animated: true)
-                    //self.performSegue(withIdentifier: "segueFromLogin", sender: nil)
-                    OperationQueue.main.addOperation {
-                        [weak self] in
-                        self?.performSegue(withIdentifier: "segueFromLogin", sender: self)
-                    }
-                    //print(responseMessage)
-                 
-                    //self.currentUserFirstName = responseMessage
-                }
-                task.resume()
-                */
                 let responseObject = sendRequest(url: server1, route: "login",
                                                  params: [
                                                     String(format: "user=%@", user_!),
